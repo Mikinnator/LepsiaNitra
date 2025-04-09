b45e671c89c0ebb450df1fc2535eb5cea635b52a
Mikinnator <141941892+Mikinnator@users.noreply.github.com>
2025-04-09 15:55:02 +0200

    Create index.tsx

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';  // Ak chceš pridať nejaké základné štýly
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
